4f1a23676bdf58b8e1dd788aac809d6cdb92e30a
MahirKothari <64363014+MahirKothari@users.noreply.github.com>
2020-06-12 19:54:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var ball;
+var database;
+var position;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballRef = database.ref('ball/position');
+    ballRef.on("value",readPosition);
+}
+
+function draw(){
+    if (position!==undefined) {
+        
+    
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-10,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(10,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-10);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+10);
+    }
+    drawSprites();
+    }
+}
+
+function changePosition(x,y){
+    database.ref('ball/position').set({
+      'x':position.x+x,
+      'y':position.y+y
+    })
+}
+
+function readPosition(data) {
+    position = data.val();
+    console.log(position);
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function mouseDragged() {
+    changePosition(mouseX,mouseY)
+}
